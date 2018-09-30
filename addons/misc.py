4965afdb35c625bf8368a2942eba297a8d1af24f
max <32982750+SizzlinRoast@users.noreply.github.com>
2018-09-30 01:27:41 +0100

    Add files via upload

---
+++ addons/misc.py
@@ -0,0 +1,114 @@
+from datetime import datetime
+
+from discord import Embed, errors, Color, Member
+from discord.ext import commands
+
+
+class Misc:
+    """
+    Miscellaneous commands
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def ping(self, ctx):
+        """Pong!"""
+        mtime = ctx.message.created_at
+        currtime = datetime.now()
+        latency = currtime - mtime
+        ptime = str(latency.microseconds / 1000.0)
+        await ctx.send(":ping_pong:! Pong! Response time: {} ms".format(ptime))
+        return
+
+    @commands.command(pass_context=True, aliases=['mc'])
+    async def membercount(self, ctx):
+        """Prints current member count"""
+        await ctx.send("{} currently has {} members!"
+                       "".format(ctx.guild.name, len(ctx.guild.members)))
+        return
+
+    @commands.command()
+    async def about(self, ctx):
+        """About elsebot."""
+        await ctx.send("View my source code here: https://github.com/SizzlinRoast/elsebot")
+
+    @commands.has_permissions(manage_messages=True)
+    @commands.command()
+    async def clear(self, ctx, amount):
+        """Clears a given amount of messages. (Mods only)"""
+
+        channel = ctx.message.channel
+        try:
+            n = int(amount) + 1
+        except ValueError:
+            return await ctx.send("Please mention a valid amount of messages!")
+
+        try:
+            await channel.purge(limit=n)
+            await ctx.send("🗑️ Cleared {} messages in this channel!".format(amount))
+            try:
+                emb = Embed(title="Messages Cleared", colour=Color.red())
+                emb.add_field(
+                    name="Mod:", value=ctx.message.author, inline=True)
+                emb.add_field(name="Channel:",
+                              value=ctx.message.channel, inline=True)
+                emb.add_field(name="Amount:", value=amount, inline=True)
+                logchannel = self.bot.logs_channel
+                await logchannel.send("", embed=emb)
+            except errors.Forbidden:
+                await ctx.send("💢 I dont have permission to do this.")
+
+        except errors.Forbidden:
+            await ctx.say("💢 I don't have permission to do this.")
+
+    @commands.command()
+    async def userinfo(self, ctx):
+        try:
+            member = ctx.message.mentions[0]
+        except IndexError:
+            member = ctx.message.author
+        uname = member.name
+        discrim = member.discriminator
+        Uid = member.id
+        displayName = member.display_name
+        isBot = member.bot
+        cTime = member.created_at
+        jTime = member.joined_at
+        str1 = "{}#{}".format(uname, discrim)
+        str2 = "{}".format(Uid)
+        if displayName == uname:
+            displayName = "None"
+        str3 = "{}".format(displayName)
+        str4 = "{}".format(str(isBot))
+        str5 = "{}".format(cTime)
+        str6 = "{}".format(jTime)
+        emb = Embed(title="Userinfo", color=0x00ff00)
+        emb.add_field(name="Member", value=str1 + "\n", inline=True)
+        emb.add_field(name="ID", value=str2 + "\n", inline=True)
+        emb.add_field(name="Nickname", value=str3 + "\n", inline=True)
+        emb.add_field(name="Bot?", value=str4 + "\n", inline=True)
+        emb.add_field(name="Account Created", value=str5 + "\n", inline=True)
+        emb.add_field(name="Joined Server", value=str6 + "\n", inline=True)
+        emb.set_thumbnail(url=member.avatar_url)
+        await ctx.send("", embed=emb)
+
+    @commands.command()
+    async def bean(self, ctx, member: Member=None, *, reason: str=""):
+        """Ban a member. (Staff Only)"""
+        if not member:
+            await ctx.send("Please mention a user.")
+            return
+        if member == ctx.message.author:
+            await ctx.send("You cannot ban yourself!")
+            return
+        elif ctx.me is member:
+            await ctx.send("I am unable to ban myself to prevent stupid mistakes.\n"
+                           "Please ban me by hand!")
+            return
+        else:
+            await ctx.send("I've banned {}.".format(member))
+
+def setup(bot):
+    bot.add_cog(Misc(bot))
---
+++ addons/speak.py
@@ -0,0 +1,146 @@
+from json import load, dump
+from discord import TextChannel, errors, abc, Embed, Color
+from discord.ext import commands
+
+
+class Speak:
+    """Give the bot a voice"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        try:
+            with open("database/ignored_users.json", "r") as config:
+                self.ignored_users = load(config)
+        except FileNotFoundError:
+            self.ignored_users = {"users": []}
+            with open("database/ignored_users.json", "w") as config:
+                dump(self.ignored_users, config, indent=4,
+                     sort_keys=True, separators=(',', ':'))
+
+    @commands.has_permissions(manage_messages=True)
+    @commands.command()
+    async def speak(self, ctx, destination: TextChannel, *, message: str):
+        """Make the bot speak (Staff Only)"""
+        await ctx.message.delete()
+        await destination.send(message)
+        try:
+            emb = Embed(title="Message Sent", colour=Color.orange())
+            emb.add_field(name="Mod:", value=ctx.message.author, inline=True)
+            emb.add_field(name="Send from:",
+                          value=ctx.message.channel, inline=True)
+            emb.add_field(name="Send To:", value=destination, inline=True)
+            emb.add_field(name="Message:", value=message, inline=True)
+            logchannel = self.bot.logs_channel
+            await logchannel.send("", embed=emb)
+        except errors.Forbidden:
+            await ctx.send("💢 I dont have permission to do this.")
+
+    async def memberDM(self, ctx, member, message):
+        """Check for various parameters before DM'ing a member"""
+        try:
+            if ctx.message.attachments:
+                attachments = " ".join(
+                    attachment.url for attachment in ctx.message.attachments)
+                message = "{} {}".format(message, attachments)
+            else:
+                if message == '':
+                    return await ctx.send("You cannot send empty messages!")
+                else:
+                    await ctx.message.delete()
+            if len(message) > 2000:
+                await member.send(message[:2000])
+                await member.send(message[2000:])
+            else:
+                await member.send(message)
+        except errors.Forbidden:
+            await self.bot.logs_channel.send("Couldn't send message to {}.".format(member.mention))
+
+    @commands.has_permissions(manage_messages=True)
+    @commands.command()
+    async def dm(self, ctx, member, *, message=''):
+        """DM a user. (Staff Only)"""
+
+        member = ctx.message.mentions[0]
+        await self.memberDM(ctx, member, message)
+        author = ctx.message.author
+        logOutput = "{} --> 📤 --> {} | \n".format(author, member)
+        logOutput += "Message Content: {}".format(message)
+        dmchannel = self.bot.botdms_channel
+        await dmchannel.send(logOutput)
+
+    # Log incoming dms if user is not ignored
+    async def on_message(self, message):
+        if isinstance(message.channel, abc.PrivateChannel):
+            author = message.author
+            if message.author.id == self.bot.user.id:
+                pass
+            elif message.author.id in self.ignored_users:
+                ignored_user_message = ("Sorry, your message `{}` could not be delivered due to "
+                                        "you being blocked from messaging the bot. If you believe "
+                                        "this is in error, too fucking bad."
+                                        "".format(message.content))
+                await author.send(ignored_user_message)
+            else:
+                dmchannel = self.bot.botdms_channel
+                if message.attachments == []:
+                    logOutput = "{} | {} 📨 {}\n".format(
+                        author, author.id, self.bot.user)
+                    logOutput += "Message Content: {}".format(message.content)
+                    await dmchannel.send(logOutput)
+                else:
+                    logOutput = "{} | {} 📨 {}\n".format(
+                        author, author.id, self.bot.user)
+                    logOutput += "Message Content: {}\n".format(
+                        message.content)
+                    logOutput += "Attachments: \n"
+                    for attachment in message.attachments:
+                        logOutput += "{}\n".format(attachment.url)
+                    await dmchannel.send(logOutput)
+
+    @commands.has_permissions(manage_messages=True)
+    @commands.command()
+    async def ignore(self, ctx, member):
+        """
+        Ignore DM's from a user (Staff Only)
+        If you use .ignore list, it will list all ignored members instead.
+        """
+
+        if member == "list":
+            if len(self.ignored_users["users"]) > 0:
+                embed = Embed(title="List of ignored users",
+                              color=Color.blue())
+
+                description = ""
+                for i in self.ignored_users["users"]:
+                    try:
+                        u = self.bot.get_user(i)
+                        description += "- {} ({}#{})\n".format(u.id,
+                                                               u.display_name, u.discriminator)
+                    except errors.CommandInvokeError:
+                        description += "- {}\n".format(i)
+
+                embed.description = description
+                await ctx.send("", embed=embed)
+                return
+            else:
+                await ctx.send("There are no ignored users!")
+                return
+        else:
+            try:
+                member = ctx.message.mentions[0]
+            except IndexError:
+                await ctx.send("Please mention a user.")
+            if member.id in self.ignored_users["users"]:
+                self.ignored_users["users"].remove(member.id)
+                await ctx.send("Removed {} from ignored users.".format(member.mention))
+            else:
+                self.ignored_users["users"].append(member.id)
+                await ctx.send("Added {} to ignored users.".format(member.mention))
+
+            with open("database/ignored_users.json", "w") as config:
+                dump(self.ignored_users, config, indent=4,
+                     sort_keys=True, separators=(',', ':'))
+
+
+def setup(bot):
+    bot.add_cog(Speak(bot))
